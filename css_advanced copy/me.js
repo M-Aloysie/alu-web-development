095fdba1b98fe25aa7fe29c63505b2bc763b4f19
M-Aloysie <a.murekatet@alustudent.com>
2024-05-16 19:59:16 -0400

    New Commit

---
+++ css_advanced copy/me.js
@@ -0,0 +1,16 @@
+
+const $ = window.$;
+$('DIV#red_header').click(function () {
+  $('HEADER').css('color', '#FF0000');
+});
+
+
+$('DIV#red_header').click(function () {
+  $('HEADER').addClass('red');
+});
+/*document.querySelector("html").addEventListener("click", function multiply(num1, num2) {
+    let result = num1 * num2;
+    return result;
+  })
+
+*/
