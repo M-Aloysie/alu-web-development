5c9707f98d6324d5676ec15f72b6ce776f329516
M-Aloysie <a.murekatet@alustudent.com>
2024-06-06 10:28:44 -0400

    new commit

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -13,7 +13,7 @@ app.url_map.strict_slashes = False
 @app.route('/')
 def hello_world():
     """hello world"""
-    return jsonify({"message": "Bienvenue"})
+    return jsonify({"message": "Believeme"})
 
 
 @app.route('/users', methods=['POST'])
